5fd939f70d1a46388c2692613e3503b6a16811cf
Francisco Mateo <contacto@fmateo.es>
2019-12-12 11:37:08 +0000

    Volcado de la API
    Iniciando la aplicación para que lea los datos de la API ("https://reqres.in/api/users") tal y como marca el ejercicio.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react'
+
 // import logo from './logo.svg';
 import './App.css';
 
 import RemoteConsumer from './components/RemoteConsumer';
 
-function App() {
-  return (
-    <div className="App">
-      <RemoteConsumer url="/api/url" />
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    fetch('https://reqres.in/api/users')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ users: data.data })
+    })
+    .catch(console.log)
+  }
+
+  render (){
+    return (
+      <div className="App">
+        <RemoteConsumer users={this.state.users} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/RemoteConsumer.js
+++ src/components/RemoteConsumer.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-function RemoteConsumer(props) {
+const RemoteConsumer = ({ users }) => {
   return (
-    <div className="RemoteConsumer">
-      <p>Consuming {props.url}</p>
-      {/* WRITE YOUR CODE HERE */}
+    <div className="users-panel">
+      <center><h1>Users</h1></center>
+      {users.map((user) => (
+        <div className="card" key={user.id}>
+          <div className="card-body">
+            <div className="card-avatar">
+              <img src={user.avatar}></img>
+            </div>
+            <h4 className="card-title">{user.first_name} {user.last_name}</h4>
+            <div className="card-email"> <a href={"mailto:" + user.email}>email</a> </div>
+          </div>
+        </div>
+      ))}
     </div>
-  );
-}
+  )
+};
 
-export default RemoteConsumer;
+export default RemoteConsumer
